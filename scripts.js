3a7cd4fca11fb211f33d0c3751af63ad23626b90
Ezra Chiang <ezrachiang@Ezras-MBP.netgear.com>
2016-11-06 16:08:22 -0800

    complete objects

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
 //Use this file to implement Part One of your project
 var animal = {};
+
+animal.username = "Mittens";
+
+animal['tagline'] = 'Pet me!';
+
+var noises = [];
+
+animal.noises = noises;
+
+var count = 0;
+for (var key in animal) {
+  count++;
+  if (key === 'username') {
+      console.log('hi my name is ' + animal[key]);
+  } else if (key === 'tagline') {
+      console.log('I like to say ' + animal[key]);
+  }
+}
