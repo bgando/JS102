0085f7f6043cdbd8d8e5b5248e99f70def8912f7
[01;31m[K"name"[m[K: "Woojae Yi <[01;31m[K"email"[m[K: null>
2017-03-21 22:13:36 -0700

    Add animal object and loop through its properties

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
 //Use this file to implement Part One of your project
 
-
+//Animal User Model
+//Object
+var animal = {};
+animal.username = "rocky";
+animal['tagline']="breh";
+var noises = [];
+animal['noises'] = noises;
+//Loops
+var count = 0;
+for(key in animal){
+  count++;
+  console.log(count);
+  if(key=="username"){
+    console.log("Hi my name is "+animal[key]);
+  }
+  else if(key=="tagline"){
+    console.log("I like to say "+animal[key]);
+  }
+};
