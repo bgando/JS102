dcc9d1e3bf99bbe0f9f03c9bfb141d12be2fadb6
Kanagu <Kanagu.2210@gmail.com>
2020-12-18 21:41:27 +1100

    Initial version

---
+++ objects.js
@@ -0,0 +1,22 @@
+
+
+var animal = {};
+animal.username = 'Mittens';
+animal['tagline'] = 'Pet me!';
+
+var noises = [];
+animal.noises = noises;
+
+console.log(animal);
+
+var count = 0;
+
+for (var key in animal) {
+    count++;
+    if (key === username) {
+        console.log('Hi my name is' + animal[key]);
+    }
+    elseif(key === 'tagline'){
+        console.log('I like to say' + animal[key]);
+    }
+}
